813b369cc73cc3d67ed71b9e117830fad69ff993
Yasel Suárez <yaselsuarez@gmail.com>
2021-04-25 13:28:37 +0200

    Creación de la clase Calculadora por Yasel Liván Suárez Planche

---
+++ src/ylspcalculadora/Calculadora.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ylspcalculadora;
+
+/**
+ *
+ * @author PC-11
+ */
+public class Calculadora {
+    public double suma(double num1, double num2){
+        return num1 + num2;
+    }
+    
+    public double resta(double num1, double num2){
+        return num1 - num2;
+    }
+    
+    public double multiplicacion(double num1, double num2){
+        return num1 * num2;
+    }
+    
+    public double division(double num1, double num2){
+        return num1 / num2;
+    }
+     
+     
+}
