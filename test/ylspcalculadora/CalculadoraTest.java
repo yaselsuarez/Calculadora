89920f314b2444ba73515b552e15874b89c34cfb
Yasel Suárez <yaselsuarez@gmail.com>
2021-04-25 14:06:46 +0200

    Modificación del test generado por defecto por Yasel Liván Suárez Planche

--- test/ylspcalculadora/CalculadoraTest.java
+++ test/ylspcalculadora/CalculadoraTest.java
@@ -43,10 +43,10 @@ public class CalculadoraTest {
     @Test
     public void testSuma() {
         System.out.println("suma");
-        double num1 = 0.0;
-        double num2 = 0.0;
+        double num1 = 4.0;
+        double num2 = 4.0;
         Calculadora instance = new Calculadora();
-        double expResult = 0.0;
+        double expResult = 8.0;
         double result = instance.suma(num1, num2);
         assertEquals(expResult, result, 0.0);
         
@@ -58,10 +58,10 @@ public class CalculadoraTest {
     @Test
     public void testResta() {
         System.out.println("resta");
-        double num1 = 0.0;
-        double num2 = 0.0;
+        double num1 = 4.0;
+        double num2 = 4.0;
         Calculadora instance = new Calculadora();
-        double expResult = 0.0;
+        double expResult = 8.0;
         double result = instance.resta(num1, num2);
         assertEquals(expResult, result, 0.0);
        
@@ -73,10 +73,10 @@ public class CalculadoraTest {
     @Test
     public void testMultiplicacion() {
         System.out.println("multiplicacion");
-        double num1 = 0.0;
-        double num2 = 0.0;
+        double num1 = 4.0;
+        double num2 = 4.0;
         Calculadora instance = new Calculadora();
-        double expResult = 0.0;
+        double expResult = 8.0;
         double result = instance.multiplicacion(num1, num2);
         assertEquals(expResult, result, 0.0);
         
@@ -88,10 +88,10 @@ public class CalculadoraTest {
     @Test
     public void testDivision() {
         System.out.println("division");
-        double num1 = 0.0;
-        double num2 = 0.0;
+        double num1 = 4.0;
+        double num2 = 4.0;
         Calculadora instance = new Calculadora();
-        double expResult = 0.0;
+        double expResult = 8.0;
         double result = instance.division(num1, num2);
         assertEquals(expResult, result, 0.0);
         
